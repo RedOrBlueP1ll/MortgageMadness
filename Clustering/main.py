50e091ed94e4c0a5c9f7d0c3e05c493b64b7f3e3
stelatopalova <56160328+stelatopalova@users.noreply.github.com>
2021-11-28 19:03:05 +0100

    Centroids for KMeans

--- Clustering/main.py
+++ Clustering/main.py
@@ -1,19 +1,19 @@
 from Kmeans import Kmeans
 from DF_edit import DF_edit
 import pandas as pd
-import matplotlib.pyplot as plt
-from matplotlib.pyplot import hist
 
 def main():
-    df_transpose = pd.read_csv('Training data/article_index_256.csv', header=None)
-    df_tensors = pd.read_csv('Training data/sentence_tensors_256.csv', header=None)
+    # only change df_transpose and df_tensors
+    # (note: the tensors with 383 dimensions have header while the others don't, hence different functions!)
+    df_transpose = pd.read_csv('Training data/article_index_128.csv', header=None)
+    df_tensors = pd.read_csv('Training data/sentence_tensors_128.csv', header=None)
     df_383 = pd.read_csv("Training data/tensorsBERT_383.csv")
 
     df_obj = DF_edit()
     article_tensor_df = df_obj.art_tens(df_transpose, df_tensors) # create a new csv with the article and the tensor
 
 
-    num_of_clusters = 51
+    num_of_clusters = 10
 
     print("Performing KMeans...")
     kmeans_model = Kmeans()
@@ -22,7 +22,7 @@ def main():
     full_df = pd.read_csv('Results Data/cluster_article_tensor.csv')
 
     pd.set_option('display.max_rows', None)
-    print(full_df.groupby(['article', 'cluster']).sum()) # see how many clusters are in an article
+    #print(full_df.groupby(['article', 'cluster']).sum()) # see how many clusters are in an article
 
     #print("Performing Elbow Method...")
     #kmeans_model.elbow_method(df_768)
